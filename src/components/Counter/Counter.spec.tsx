f4d9388dff9a6b0f3f013a8f99fa0ea15df5b050
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-26 18:51:05 -0300

    Add Counter component todo tasks

---
+++ src/components/Counter/Counter.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+
+// import { Counter } from '.'
+
+describe('Counter component', () => {
+  it.todo('should render with success')
+  it.todo('should decrement -1 on counter when on click decrement button')
+  it.todo('should increment +1 on counter when on click increment button')
+})
