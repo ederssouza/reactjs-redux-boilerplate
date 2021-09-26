5a352e372c9ce502dd6e80b7c4f8cc4e292f38c4
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-26 20:05:47 -0300

    Add tests

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -1,9 +1,45 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
 
-// import { Counter } from '.'
+import { Counter } from '.'
+import { store } from '../../redux/store'
+
+const wrapper = () => render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+)
 
 describe('Counter component', () => {
-  it.todo('should render with success')
-  it.todo('should decrement -1 on counter when on click decrement button')
-  it.todo('should increment +1 on counter when on click increment button')
+  it('should render with success', () => {
+    wrapper()
+
+    const $counterValue = screen.getByTestId('counter-value')
+
+    expect($counterValue.textContent).toEqual('0')
+  })
+
+  it('should decrement -1 on counter when on click decrement button', () => {
+    wrapper()
+
+    const $counterValue = screen.getByTestId('counter-value')
+    const $decrementButton = screen.getByTestId('decrement-button')
+
+    fireEvent.click($decrementButton)
+    fireEvent.click($decrementButton)
+
+    expect($counterValue.textContent).toEqual('-2')
+  })
+
+  it('should increment +1 on counter when on click increment button', () => {
+    wrapper()
+
+    const $counterValue = screen.getByTestId('counter-value')
+    const $incrementButton = screen.getByTestId('increment-button')
+
+    fireEvent.click($incrementButton)
+    fireEvent.click($incrementButton)
+
+    expect($counterValue.textContent).toEqual('0')
+  })
 })
